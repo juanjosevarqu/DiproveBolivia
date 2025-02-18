884c42693585bbac9a5b686398528616bc9dcef0
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-18 19:55:10 -0400

    REFACTOR - Default App Bar

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/DefaultAppBar.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/DefaultAppBar.kt
@@ -1,9 +1,11 @@
-package com.varqulabs.diproveboliviapp.core.presentation
+package com.varqulabs.diproveboliviapp.core.presentation.composables
 
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,12 +14,19 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultAppBar(
     title: String,
     modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable() (RowScope.() -> Unit) = {},
+    expandedHeight: Dp = TopAppBarDefaults.TopAppBarExpandedHeight,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        containerColor = Color(0xFFFEFEFE)
+    )
 ) {
     CenterAlignedTopAppBar(
         title = {
@@ -33,9 +42,10 @@ fun DefaultAppBar(
                 )
             }
         },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFFFEFEFE)
-        ),
+        navigationIcon = navigationIcon,
+        actions = actions,
+        expandedHeight = expandedHeight,
+        colors = colors,
         modifier = modifier
     )
 }
