6e8eced02e4ffa14fa3e6cd97ee54ba69ec44ed4
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-18 17:41:20 -0400

    REFACTOR - Center Aligned App Bar

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/DefaultAppBar.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/DefaultAppBar.kt
@@ -1,15 +1,17 @@
 package com.varqulabs.diproveboliviapp.core.presentation
 
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,7 +19,7 @@ fun DefaultAppBar(
     title: String,
     modifier: Modifier = Modifier,
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
             if (title.isNotBlank()) {
                 Text(
@@ -25,7 +27,9 @@ fun DefaultAppBar(
                     style = MaterialTheme.typography.titleLarge,
                     fontFamily = FontFamily.Serif,
                     fontWeight = FontWeight.Bold,
-                    maxLines = 1,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Center,
                 )
             }
         },
