8da85946aba6bf4e3a0d3b016b4842233379e936
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-20 21:11:17 -0400

    REFACTOR - Icon Card Item in Home Screen

--- app/src/main/java/com/varqulabs/diproveboliviapp/home/home/HomeScreen.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/home/home/HomeScreen.kt
@@ -1,36 +1,22 @@
 package com.varqulabs.diproveboliviapp.home.home
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.varqulabs.diproveboliviapp.R
 import com.varqulabs.diproveboliviapp.core.presentation.composables.DiproveCenterAppBar
-import com.varqulabs.diproveboliviapp.core.presentation.composables.DiproveDescriptionItem
 import com.varqulabs.diproveboliviapp.core.presentation.composables.DiprovePoliceBackgroundContainer
-import com.varqulabs.diproveboliviapp.core.presentation.utils.modifier.clickableSingle
 import com.varqulabs.diproveboliviapp.home.home.components.IconCardItem
 import com.varqulabs.diproveboliviapp.navigation.HomeScreens
 
@@ -75,7 +61,7 @@ fun HomeScreen(
             ) {
                 items(homeScreens) { screen ->
                     IconCardItem(
-                        modifier = Modifier.size(124.dp),
+                        modifier = Modifier.size(148.dp),
                         icon = screen.icon,
                         text = screen.name,
                         onClick = { onClickRouteItem(screen.route) }
--- app/src/main/java/com/varqulabs/diproveboliviapp/home/home/components/IconCardItem.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/home/home/components/IconCardItem.kt
@@ -6,16 +6,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.Hyphens
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -31,33 +36,39 @@ fun IconCardItem(
 ) {
     Column(
         modifier = modifier
-            .background(
-                color = Color(0xB7E8F5E9),
-                shape = RoundedCornerShape(24.dp),
-            )
+            .clip(RoundedCornerShape(24.dp))
+            .background(Color(0xB7E8F5E9))
             .border(
-                width = 1.dp,
-                color = Color(0xFF177437),
+                width = 1.5.dp,
+                color = Color(0xFF827717),
                 shape = RoundedCornerShape(24.dp),
             )
-            .padding(horizontal = 8.dp)
-            .clickableSingle(onClick = onClick),
+            .clickableSingle(onClick = onClick)
+            .padding(horizontal = 12.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
         Icon(
             imageVector = ImageVector.vectorResource(icon),
-            contentDescription = "Navegar a pantalla"
+            contentDescription = "Navegar a pantalla",
+            tint = Color(0xFFD9AB24),
+            modifier = Modifier
+                .size(48.dp)
         )
 
         Text(
             text = text,
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
             maxLines = 3,
             overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
+            style = TextStyle(
+                hyphens = Hyphens.Auto,
+                fontSize = 16.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFF827717),
+            ),
         )
     }
 }
