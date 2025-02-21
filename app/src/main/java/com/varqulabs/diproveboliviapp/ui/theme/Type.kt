de7a3daa3c4f137ebe1b67f879bbc7368440696e
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-21 12:08:13 -0400

    ADD - Bricolage Grotesque Font

--- app/src/main/java/com/varqulabs/diproveboliviapp/ui/theme/Type.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.varqulabs.diproveboliviapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.varqulabs.diproveboliviapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,14 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val BricolageGrotesque = FontFamily(
+    Font(R.font.bricolage_grotesque_regular, FontWeight.Normal),
+    Font(R.font.bricolage_grotesque_medium, FontWeight.Medium),
+    Font(R.font.bricolage_grotesque_semi_bold, FontWeight.SemiBold),
+    Font(R.font.bricolage_grotesque_bold, FontWeight.Bold),
+    Font(R.font.bricolage_grotesque_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.bricolage_grotesque_light, FontWeight.Light),
+    Font(R.font.bricolage_grotesque_extra_light, FontWeight.ExtraLight),
 )
