d2efec3658d0f0f41ab1690ad6dbbf9f16148951
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-24 00:26:52 -0400

    MOVE - To Contants Diprove Images Banners

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/domain/Constants.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/domain/Constants.kt
@@ -1,5 +1,32 @@
 package com.varqulabs.diproveboliviapp.core.domain
 
+import com.varqulabs.diproveboliviapp.R
+
 const val BANK_NUMBER = "1-4717944"
 const val BANK_NUMBER_COPY = "14717944"
 const val DIPROVE_CBBA_WHATSAPP = "59164912407"
+
+val imagesVerticalCarruselDiprove = listOf(
+    R.drawable.diprove_carrusel_vertical_1,
+    R.drawable.diprove_carrusel_vertical_2,
+    R.drawable.diprove_carrusel_vertical_3,
+    R.drawable.diprove_carrusel_vertical_4,
+    R.drawable.diprove_carrusel_vertical_5,
+    R.drawable.diprove_carrusel_vertical_6,
+    R.drawable.diprove_carrusel_vertical_7,
+    R.drawable.diprove_carrusel_vertical_8,
+)
+
+val imagesHorizontalCarruselDiprove = listOf(
+    R.drawable.diprove_carrusel_horizontal_1,
+    R.drawable.diprove_carrusel_horizontal_2,
+    R.drawable.diprove_carrusel_horizontal_3,
+    R.drawable.diprove_carrusel_horizontal_4,
+    R.drawable.diprove_carrusel_horizontal_5,
+    R.drawable.diprove_carrusel_horizontal_6,
+    R.drawable.diprove_carrusel_horizontal_7,
+    R.drawable.diprove_carrusel_horizontal_8,
+    R.drawable.diprove_carrusel_horizontal_9,
+    R.drawable.diprove_carrusel_horizontal_10,
+    R.drawable.diprove_carrusel_horizontal_11,
+)
