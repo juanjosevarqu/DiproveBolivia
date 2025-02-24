ea3f44cbeb8bbfcf5df76fd8fda611f629d3b49b
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-24 00:33:25 -0400

    FEATURE - Create Diprove Carousel Pager

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/utils/modifier/ModifierExtensions.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/utils/modifier/ModifierExtensions.kt
@@ -2,6 +2,7 @@ package com.varqulabs.diproveboliviapp.core.presentation.utils.modifier
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,9 +11,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.util.lerp
 import kotlinx.coroutines.delay
+import kotlin.math.absoluteValue
 
 fun Modifier.clickableSingle(
     enabled: Boolean = true,
@@ -39,4 +43,17 @@ fun Modifier.clickableSingle(
         indication = indication,
         interactionSource = remember { MutableInteractionSource() }
     )
+}
+
+fun Modifier.carouselTransition(
+    page: Int,
+    pagerState: PagerState,
+): Modifier {
+    val pageOffset = ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+    val transformation = lerp(
+        start = .7f,
+        stop = 1f,
+        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+    )
+    return this.graphicsLayer { alpha = transformation }
 }
