d9661d5e4e3ab76c914c39857aa4ee99eccf86bb
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-24 14:01:59 -0400

    ADD - Diprove Welcome Screen Carousel Images

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/domain/Constants.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/domain/Constants.kt
@@ -15,9 +15,15 @@ val imagesVerticalCarruselDiprove = listOf(
     R.drawable.diprove_carrusel_vertical_6,
     R.drawable.diprove_carrusel_vertical_7,
     R.drawable.diprove_carrusel_vertical_8,
-)
-
-val imagesHorizontalCarruselDiprove = listOf(
+    R.drawable.diprove_carrusel_welcome_1,
+    R.drawable.diprove_carrusel_welcome_2,
+    R.drawable.diprove_carrusel_welcome_3,
+    R.drawable.diprove_carrusel_welcome_4,
+    R.drawable.diprove_carrusel_welcome_5,
+    R.drawable.diprove_carrusel_welcome_6,
+    R.drawable.diprove_carrusel_welcome_7,
+    R.drawable.diprove_carrusel_welcome_8,
+    R.drawable.diprove_carrusel_welcome_9,
     R.drawable.diprove_carrusel_horizontal_1,
     R.drawable.diprove_carrusel_horizontal_2,
     R.drawable.diprove_carrusel_horizontal_3,
@@ -25,9 +31,7 @@ val imagesHorizontalCarruselDiprove = listOf(
     R.drawable.diprove_carrusel_horizontal_5,
     R.drawable.diprove_carrusel_horizontal_6,
     R.drawable.diprove_carrusel_horizontal_7,
-    R.drawable.diprove_analysis_intelligence_division,
     R.drawable.diprove_carrusel_horizontal_9,
-    R.drawable.diprove_vehicle_division,
     R.drawable.diprove_carrusel_horizontal_11,
 )
 
