ead33e6344cd62f67572627ebd0ab6c080f5002d
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-24 00:35:20 -0400

    FEATURE - Create Pulsating Content

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/PulsatingContent.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/PulsatingContent.kt
@@ -1,30 +1,26 @@
 package com.varqulabs.diproveboliviapp.core.presentation.composables
 
-import androidx.annotation.DrawableRes
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
 
 @Composable
-fun PulsatingImage(
-    modifier: Modifier = Modifier,
-    @DrawableRes image: Int,
-    contentDescription: String? = null,
-    pulseRange: ClosedFloatingPointRange<Float> = 1.0f..1.08f,
-    durationMillis: Int = 1000
+fun PulsatingContent(
+    pulseRange: ClosedFloatingPointRange<Float> = 1.0f..1.1f,
+    durationMillis: Int = 1000,
+    content: @Composable (Modifier) -> Unit
 ) {
 
     val infiniteTransition = rememberInfiniteTransition()
+
     val scale by infiniteTransition.animateFloat(
         initialValue = pulseRange.start,
         targetValue = pulseRange.endInclusive,
@@ -34,10 +30,8 @@ fun PulsatingImage(
         )
     )
 
-    Image(
-        painter = painterResource(id = image),
-        contentDescription = contentDescription,
-        modifier = modifier.graphicsLayer {
+    content(
+        Modifier.graphicsLayer {
             scaleX = scale
             scaleY = scale
             transformOrigin = TransformOrigin.Center
