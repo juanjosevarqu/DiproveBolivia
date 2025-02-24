2ba22880b519b7904e1947f1421d33d47a0b652d
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-24 00:34:43 -0400

    Remove unused import

--- app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/ChipItem.kt
+++ app/src/main/java/com/varqulabs/diproveboliviapp/core/presentation/composables/ChipItem.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.varqulabs.diproveboliviapp.ui.theme.BricolageGrotesque
 import com.varqulabs.diproveboliviapp.ui.theme.BricolageGrotesqueSemiCondensed
 
 @Composable
